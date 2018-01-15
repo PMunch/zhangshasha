90debbab64c937ccb2be42993def4debd8ffba6d
Peter Munch-Ellingsen <peterme@peterme.net>
2018-01-15 22:53:52 +0100

    Added comments to example

--- example.nim
+++ example.nim
@@ -1,4 +1,6 @@
 import zhsh
+
+# Create two trees to compare
 var
   test1 = "d"
   tokenizer1 = test1.tokenize()
@@ -6,6 +8,9 @@ var
   tokenizer2 = test2.tokenize()
   t1 = initTree(tokenizer1)
   t2 = initTree(tokenizer2)
+
+# Create a third tree manually, this is the same tree as t2
+var
   t3 = Tree(root:
     Node(label: "g", children: @[
       Node(label: "h", children: @[])
